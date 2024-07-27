d23b096b46fb775442234ca95611e1b6620db1e8
Victor Augusto da Silva <victor1231967@hotmail.com>
2024-07-27 19:59:16 -0300

    Estilização tela de Login

---
+++ components/login/index.js
@@ -0,0 +1,57 @@
+import InputPublico from "../inputPublico";
+import imagemEnvelope from "../../public/image/envelope.svg";
+import imagemChave from "../../public/image/chave.svg";
+import imagemLogo from "../../public/image/logo.svg";
+import Image from 'next/image';
+import Botao from '../botao'
+import Link from "next/link";
+import { useState } from "react";
+
+
+export default function Login() {
+/*Email começa vazio*/ 
+    const [email,setEmail] = useState("");
+    const [senha,setSenha] = useState("");
+
+
+    return (
+        <section className="paginaLogin paginaPublica">
+          <div className="logoContainer">
+                <Image
+                    src={imagemLogo}
+                    alt='logotipo'
+                    layout="fill"
+                />
+            </div>
+            <div className="conteudoPaginaPublica">
+                <form>
+                    <InputPublico
+                        imagem={imagemEnvelope}
+                        texto="E-mail"
+                        tipo="email"
+                        /*Ref do obj do input*/
+                        aoAlterarValor={(e) => setEmail(e.target.value)}
+                        valor={email}
+                    />
+                    <InputPublico
+                        imagem={imagemChave}
+                        texto="Senha"
+                        tipo="password"
+                        aoAlterarValor={(e) => setSenha(e.target.value)}
+                        valor={senha}
+                    />
+
+                    <Botao
+                        texto={"Login"}
+                        tipo="submit"
+                        desabilitado={false}
+                    /> 
+                </form>
+                <div className="rodapePaginaPublica">
+                <p>Não possui uma conta?</p>
+                <Link href="/cadastro"> Faça seu cadastro agora</Link>
+                </div>
+            </div>
+        </section>
+    );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,24 @@ import Botao from "../../components/botao";
 import Avatar from "../../components/avatar";
 import { UploadImagem } from "../../components/uploadImagem";
 import { useState,useRef } from "react";
-
-
+import Login from "../../components/login";
 export default function Home() {
-
 /*armazena o componente da imagem*/ 
 const[imagem,setImagem] = useState(null);
 const referenciaInput =    useRef(null);
 console.log(imagem);
 
 
+return (<> 
+
+<Login />
+
+</>);
+
+/*
   return (
     <div>
+
     <h1>Ola mundo </h1>
     <button onClick={()=> referenciaInput?.current?.click() }>Abrir seletor de arquivos</button>
     <UploadImagem 
@@ -29,7 +35,10 @@ console.log(imagem);
     <Avatar/>
     <Botao texto={`Login`}  cor='primaria'   manipularClique= {() => console.log('Botão clicado')}  />
     </div>
-  );
-} 
+  );*/ 
 
-/*cor='invertido'  pega a cor do botão e inverte*/
+
+
+
+} 
+ 
