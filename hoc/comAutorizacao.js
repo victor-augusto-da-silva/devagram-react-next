7ef563cb2a522e8b41870ac6e02c7249b1bd13f8
Victor Augusto da Silva <victor1231967@hotmail.com>
2024-08-19 11:20:58 -0300

    Estilo header e navbar

--- hoc/comAutorizacao.js
+++ hoc/comAutorizacao.js
@@ -1,28 +1,26 @@
 import { useRouter } from "next/navigation";
-import UsuarioService from "../services/UsuarioService"
+import UsuarioService from "../services/UsuarioService";
 import Cabecalho from "../components/layout/Cabecalho";
 
 const usuarioService = new UsuarioService();
 
-export default function comAutorizacao(Componente){
-    return (props) => {
+export default function comAutorizacao(Componente) {
+    return function AutorizacaoWrapper(props) {
         const router = useRouter();
 
-        if(typeof window !== 'undefined'){
-            if(!usuarioService.estaAutenticado()){
+        if (typeof window !== 'undefined') {
+            if (!usuarioService.estaAutenticado()) {
                 // substitui a url atual pela raiz
                 router.replace('/');
                 return null;
             }
             return (
-                <div> 
-                <Cabecalho />
-                <Componente {...props} />
-                </div>    
+                <div>
+                    <Cabecalho />
+                    <Componente {...props} />
+                </div>
             );
-
-     
         }
         return null;
-    }
+    };
 }
