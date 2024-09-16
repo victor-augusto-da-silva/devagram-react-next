d84f7755ac756e419dd45307814e695066095dd0
Victor Augusto da Silva <victor1231967@hotmail.com>
2024-09-16 18:24:31 -0300

    Inicio estilização ver mais

--- components/feed/Postagem.js
+++ components/feed/Postagem.js
@@ -1,13 +1,40 @@
 import Link from "next/link";
 import Avatar from "../avatar";
 import Image from "next/image";
-
 import imgCurtir from '../../public/image/curtir.svg';
 import imgCurtido from '../../public/image/curtido.svg';
 import imgComentarioAtivo from '../../public/image/comentarioAtivo.svg';
 import imgComentarioCinza from '../../public/image/comentarioCinza.svg';
+import { useState } from "react";
 
-export default function Postagem({ usuario, fotoDoPost, descricao, comentarios }) {
+const tamanhoLimiteDescricao = 90;
+
+
+export default function Postagem({
+     usuario, fotoDoPost, descricao, comentarios
+     }) {
+
+    const [tamanhoAtualDaDescricao,setTamanhoAtualDaDescricao] = useState(tamanhoLimiteDescricao);
+        const exibirDescricaoCompleta = () => {
+            setTamanhoAtualDaDescricao(Number.MAX_SAFE_INTEGER);
+        }
+    const descricaoMaiorQueLimite = () =>{
+        return descricao.length > tamanhoAtualDaDescricao
+    }
+
+
+    const obterDescricao = () =>{
+        let  mensagem = descricao.substring(0, tamanhoAtualDaDescricao);
+
+
+
+        if(descricaoMaiorQueLimite()) 
+        {
+                mensagem += '...'
+        }
+        return mensagem
+    }
+
     return (
         <div className="postagem">
             <Link href={`/perfil/${usuario.id}`}>
@@ -50,14 +77,19 @@ export default function Postagem({ usuario, fotoDoPost, descricao, comentarios }
                 <div className="descricaoDaPostagem">
                     <strong className="nomeUsuario">{usuario.nome}</strong>
                     <p className="descricao">
-                        {descricao}
+                        {obterDescricao()}
+                        {descricaoMaiorQueLimite() &&  (
+                            <span onClick={exibirDescricaoCompleta}   className="exibirDescricaoCompleta">
+                                mais
+                            </span>
+                        )}
                     </p>
                 </div>
                 <div className="comentariosDaPublicacao">
-                {comentarios.map((comentario, i) => (
+                {comentarios.map((comentarios, i) => (
                     <div className="comentario" key={i}>
-                        <strong className="comentario-nome">{comentario.nome}</strong>
-                        <p className="descricao">{comentario.mensagem}</p>
+                        <strong className="comentario-nome">{comentarios.nome}</strong>
+                        <p className="descricao">{comentarios.mensagem}</p>
                     </div>
                 ))}
             </div>
--- components/feed/index.js
+++ components/feed/index.js
@@ -15,7 +15,8 @@ export function Feed({ usuarioLogado }) {
                     avatar: null
                 },
                 fotoDoPost: 'https://www.freecodecamp.org/portuguese/news/content/images/2023/03/Ekran-Resmi-2019-11-18-18.08.13.png',
-                descricao: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+                descricao: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
+               
                 curtidas: [],
                 comentarios: [
                     {
@@ -41,7 +42,7 @@ export function Feed({ usuarioLogado }) {
                     avatar: null
                 },
                 fotoDoPost: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg',
-                descricao: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
+                descricao: 'Lorem Ipsum is simply  ',
                 curtidas: [],
                 comentarios: [
                     {
